3cb3102f6c599288eab95d5da96b8b31f609c208
smrithigUCI <123223145+smrithigUCI@users.noreply.github.com>
2024-03-03 16:13:20 -0800

    Update halfAdder.v

--- halfAdder.v
+++ halfAdder.v
@@ -1,40 +1,19 @@
-interface halfAdder_If ;
-
-  // Port List
-  
-   logic a_test;
-   logic b_test;
-   logic clk_test;
-   logic sum_test;
-   logic carry_test;
-   
-   
-
-  // Monitor Modport
-   
-   modport cntr_mp (
-                   input a_test,
-     			   input b_test,
-     			   input clk_test,
-                   output sum_test,
-                   output carry_test       
-                   );
-
-   initial begin : clock_gen
-      clk_test = 1;
-      forever #10 clk_test = ~clk_test;
-   end
-
-      
-endinterface : halfAdder_If
-
-module halfAdder(halfAdder_If.cntr_mp i);
-  reg s; reg c;
-  always@(posedge i.clk_test)
+module halfAdder(input a,
+                 input b,
+                 output sum,
+                 output carry);
+  reg clk;
+  reg sumReg,carryReg;
+  initial 
     begin
-      s<=i.a_test^i.b_test;
-      c<=i.a_test&i.b_test;
+      clk = 1'b0;
+      forever #10 clk = ~clk;
     end
-  assign i.sum_test =s;
-  assign i.carry_test =c;
+  always @(posedge clk)
+    begin
+      sumReg = a^b;
+      carryReg = a&b;
+    end
+  assign sum = sumReg;
+  assign carry = carryReg;
 endmodule
